7b6d022cc99903d8d16dff3fc13ef39358c73169
pamun1 <108357749+pamun1@users.noreply.github.com>
2023-04-10 23:07:09 -0400

    Added data_reader.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     package_dir={'pyvest': 'pyvest'},
-    packages=["pyvest", "pyvest.factor_model", "pyvest.general",
-               "pyvest.investment_universe", "pyvest.simulation"]
+    packages=["pyvest", "pyvest.data_reader", "pyvest.factor_model",
+              "pyvest.general", "pyvest.investment_universe",
+              "pyvest.simulation"]
 )
